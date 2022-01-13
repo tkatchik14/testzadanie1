f6c792d78055b3d502c692ce204dfd476d0fa706
tkatchik14 <tkatchik14@yandex.ru>
2022-01-13 22:06:54 +0300

    добавила в скрипт заполнение данными таблиц БД

--- spravochnik/scripts/tables.sql
+++ spravochnik/scripts/tables.sql
@@ -61,3 +61,17 @@ comment on column nsi.dictionary_item.item_value is 'значение элеме
 create index ix_dictionary_id on nsi.dictionary_item (dictionary_id)
 ;
 grant all on nsi.dictionary to nsi_user;
+;
+INSERT INTO nsi.dictionary (id, sprid, name, shortname, description, version, datefrom, dateto) VALUES (6, 'SOCSTATUS', 'справочник социальный статус', 'статус', null, '1.1', '2022-01-03', '2022-01-10');
+INSERT INTO nsi.dictionary (id, sprid, name, shortname, description, version, datefrom, dateto) VALUES (5, 'SOCSTATUS', 'справочник социальный статус', 'статус', null, '2.0', '2022-01-10', null);
+INSERT INTO nsi.dictionary (id, sprid, name, shortname, description, version, datefrom, dateto) VALUES (4, 'SOCSTATUS', 'справочник социальный статус', 'статус', null, '1.0', '2022-01-01', '2022-01-03');
+INSERT INTO nsi.dictionary (id, sprid, name, shortname, description, version, datefrom, dateto) VALUES (3, 'GENDER', 'справочник пол человека', 'пол', null, '1.0', '2022-01-02', null);
+INSERT INTO nsi.dictionary (id, sprid, name, shortname, description, version, datefrom, dateto) VALUES (7, 'TEST1', 'TESTSPR', 'TESTSPR', 'тестовый справочник', '1.0', '2022-01-07', '2022-01-07');
+INSERT INTO nsi.dictionary_item (id, dictionary_id, parent_id, item_code, item_value) VALUES (4, 3, null, 'м', 'мужской');
+INSERT INTO nsi.dictionary_item (id, dictionary_id, parent_id, item_code, item_value) VALUES (5, 3, null, 'ж', 'женский');
+INSERT INTO nsi.dictionary_item (id, dictionary_id, parent_id, item_code, item_value) VALUES (6, 4, null, 'рр', 'работает');
+INSERT INTO nsi.dictionary_item (id, dictionary_id, parent_id, item_code, item_value) VALUES (7, 4, null, 'нр', 'не работает');
+INSERT INTO nsi.dictionary_item (id, dictionary_id, parent_id, item_code, item_value) VALUES (8, 5, null, 'рр', 'работает');
+INSERT INTO nsi.dictionary_item (id, dictionary_id, parent_id, item_code, item_value) VALUES (9, 5, null, 'нр', 'не работает');
+INSERT INTO nsi.dictionary_item (id, dictionary_id, parent_id, item_code, item_value) VALUES (10, 5, null, 'пс', 'пенсионер');
+INSERT INTO nsi.dictionary_item (id, dictionary_id, parent_id, item_code, item_value) VALUES (14, 6, null, 'нр', 'не работает');
